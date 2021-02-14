ebd1dd83d28a173bc9750558e2a3c252f43b4ccd
the-stranded-alien <thestrandedalien.mysticdark@gmail.com>
2021-02-15 00:03:05 +0530

    GET METHOD - REST API

---
+++ src/main/java/com/sahil/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.sahil.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// controller
+@RestController
+public class HelloWorldController {
+    // GET
+    // URI - /hello-world
+    // method = "Hello World"
+    @GetMapping(path="/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    // hello-world-bean
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
+
